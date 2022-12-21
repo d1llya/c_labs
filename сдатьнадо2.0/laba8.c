50c7aaa9259d86cfd80b57236bf1d15bb4f3bc34
temnoda <115697567+d1llya@users.noreply.github.com>
2022-12-21 21:11:14 +0500

    Update laba8.c

--- сдатьнадо2.0/laba8.c
+++ сдатьнадо2.0/laba8.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <termios.h>
+#include <unistd.h>
 
+int mygetch()
+{
+    struct termios old_terminal_attr, new_terminal_attr;
+    int symbol;
+
+    tcgetattr(STDIN_FILENO, &old_terminal_attr);
+    new_terminal_attr = old_terminal_attr;
+    new_terminal_attr.c_lflag &= ~(ICANON | ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_terminal_attr);
+    symbol = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal_attr);
+
+    return symbol;
+}
+
+int encrypt_cesar(int symbol, int shift)
+{
+    if (islower(symbol))
+    {
+        symbol = (symbol - 'a' + shift) % 26 + 'a';
+    }
+    else if (isupper(symbol))
+    {
+        symbol = (symbol - 'A' + shift) % 26 + 'A';
+    }
+    return symbol;
+}
+
+int main()
+{
+    const int shift = 1;
+    int symbol;
+
+    while ((symbol = mygetch()) != '\n')
+    {
+        symbol = encrypt_cesar(symbol, shift);
+        printf("%c", symbol);
+    }
+    printf("\n");
+
+    return 0;
+}
