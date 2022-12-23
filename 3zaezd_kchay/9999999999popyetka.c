fd9d27d2016fdb3871c6aa98b752b7619afbfdcf
temnoda <115697567+d1llya@users.noreply.github.com>
2022-12-24 00:08:35 +0500

    Create 9999999999popyetka.c

---
+++ 3zaezd_kchay/9999999999popyetka.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char str[100], newstr[100]; 
+    printf("Введите строку: ");
+    fgets(str, 100, stdin);
+    int j = 0;
+    for (int i = 0; i < strlen(str); i++) 
+    {
+        int count = 1;
+        while (i < strlen(str) - 1 && str[i + 1] == str[i]) 
+        {
+            if (str[i] == ' ') 
+            {
+                break;
+            }
+        count++;
+        i++;    
+        }
+    if (count != 1) 
+    {
+        newstr[j++] = '0' + count;
+        newstr[j++] = str[i - 1];
+    }
+    else 
+    {
+        newstr[j++] = str[i];
+    }   
+}
+for (int i = 0; i < j; i++) 
+{
+    printf("%c", newstr[i]);
+}
+return 0;
+}
+
+// gcc laba91.c -Wall -lm -o lab911
