435fbcc5d9a21f68911ae4b62f369e85314e764d
temnoda <115697567+d1llya@users.noreply.github.com>
2022-12-23 14:26:45 +0500

    Create laba999_00.c

---
+++ 3zaezd_kchay/laba999_00.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+void revStr(char *start, char *end)
+{
+    for (char *st = start, *en = end - 1; st < en; ++st, --en)
+    {
+        char ch = *st;
+        *st = *en;
+        *en = ch;
+    }
+}
+
+int intToStr(char *str, int number)
+{
+    char *end = str;
+    while (number > 0)
+    {
+        *end = number % 10 + '0';
+        number /= 10;
+        ++end;
+    }
+    *end = '\0';
+    revStr(str, end);
+    return (end - str) / sizeof(char);
+}
+
+void zipStr(char *str)
+{
+    int left = 0;
+    int size = 0;
+    char current_letter = str[0];
+
+    for (int i = 1; str[i - 1] != '\0'; ++i)
+    {
+        if (str[i] != current_letter)
+        {
+            int n = i - left;
+            if (n != 1)
+            {
+                n = intToStr(str + size, n);
+                size += n;
+            }
+            str[size] = current_letter;
+            ++size;
+            left = i;
+            current_letter = str[i];
+        }
+    }
+    str[size] = '\0';
+}
+
+int main()
+{
+    char str[1024];
+    printf("Enter string  -> ");
+    fgets(str, 1024, stdin);
+
+    zipStr(str);
+    
+    printf("Result string -> ");
+    fputs(str, stdout);
+
+    return 0;
+}
